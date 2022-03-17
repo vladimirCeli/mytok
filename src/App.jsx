d1a0b74fc826d13540c0034fcb1e6e93010ba6c2
vladimirCeli <vladimir.celi@unl.edu.ec>
2022-03-17 17:39:18 -0500

    Proyecto mytok

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+    <main>
+          MyTOK
+    </main>
+    </div>
+  )
+}
+
+export default App
